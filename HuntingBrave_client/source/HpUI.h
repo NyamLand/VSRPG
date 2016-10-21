008fcf97cf41552849f27a2b60a14eb1f6c4ab57
たかやん <takayan1994@gmail.com>
2016-10-21 15:55:25 +0900

    uitool & hpUI tester

---
+++ HuntingBrave_client/source/HpUI.h
@@ -0,0 +1,26 @@
+
+#pragma once
+
+//***************************************************************
+//
+//	HpUIUIクラス
+//
+//***************************************************************
+
+//	include
+
+//	class
+class HpUI
+{
+private:
+
+public:
+	//	初期化・解放
+	HpUI(void);
+	~HpUI(void);
+
+	//	更新・描画
+	void	Update(void);
+	void	Render(void);
+};
+
