81b0d691645552867f5ba4639e6f1864ab6a97c0
高鳥義行 <wtpmjgdadgjptw@gmail.com>
2016-12-01 16:19:15 +0900

    サーバー側シーン切り替え

---
+++ HuntingBrave_server/Source/sceneMatching.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+//************************************************************************
+//
+//	sceneMatchingクラス
+//
+//************************************************************************
+
+//	include
+#include	"Scene.h"
+
+//	class
+class sceneMatching : public Scene
+{
+private:
+public:
+	//	初期化・解放
+	bool	Initialize(void);
+	~sceneMatching(void);
+
+	//	更新
+	void	Update(void);
+};
