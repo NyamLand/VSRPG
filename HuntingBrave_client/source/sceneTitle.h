8171e7f888548d07b61b71457f646a9b550eab96
たかやん <takayan1994@gmail.com>
2016-12-02 15:00:15 +0900

    image update

--- HuntingBrave_client/source/sceneTitle.h
+++ HuntingBrave_client/source/sceneTitle.h
@@ -15,10 +15,13 @@ class sceneTitle : public Scene
 private:
 	iex2DObj*	bg;
 	iex2DObj*	lovelive;
+	iex2DObj*	chanolive;
 
 	bool		pushState;
 	float		percentage;
+	float		percentage2;
 	float		alpha;
+	float		alpha2;
 public:
 	//	‰Šú‰»E‰ğ•ú
 	bool	Initialize( void )override;
